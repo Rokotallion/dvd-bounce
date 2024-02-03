8654ba45b8a1a814a322bc92bebb267f6fbe101c
Kolonel Roberts <79259221+Rokotallion@users.noreply.github.com>
2024-02-03 22:12:21 +0800

    update main

--- js/main.js
+++ js/main.js
@@ -1,79 +1,109 @@
+// random number function
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+// start of Code
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.OrthographicCamera();
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( 800, 800 );
 document.body.appendChild( renderer.domElement );
 
-// Cube
-const cubeGeometry = new THREE.BoxGeometry();
-const cubeMaterial = new THREE.MeshPhongMaterial( { color: 0x00ff00, specular: 'blue' } );
-const cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
+// plane
+const dvdGeometry = new THREE.PlaneGeometry(0.3, 0.3);
+const dvdMaterial = new THREE.MeshPhongMaterial( { color: 0x00ff00, side: THREE.DoubleSide } );
+const dvd = new THREE.Mesh( dvdGeometry, dvdMaterial );
 
-// Torus
-const ringGeometry = new THREE.TorusGeometry( 1, 0.2, 16, 100 ); 
-const ringMaterial = new THREE.MeshPhongMaterial( { color: 0xffff00, specular: 'blue' } ); 
-const ring = new THREE.Mesh( ringGeometry, ringMaterial ); 
-
-// Cylinder
-const canGeometry = new THREE.CylinderGeometry( 0.5, 0.5, 2, 32 ); 
-const canMaterial = new THREE.MeshPhongMaterial( { color: 'blue', specular: 'pink' } ); 
-const can = new THREE.Mesh( canGeometry, canMaterial ); 
-
-// Cone
-const coneGeometry = new THREE.ConeGeometry( 0.7, 2, 64 ); 
-const coneMaterial = new THREE.MeshPhongMaterial( { color: 'red', specular: 'pink' } ); 
-const cone = new THREE.Mesh(coneGeometry, coneMaterial );
-
-// Sphere
-const sphereGeometry = new THREE.SphereGeometry( 1, 16, 8 ); 
-const sphereMaterial = new THREE.MeshPhongMaterial( { color: 'pink', specular: 'yellow' } ); 
-const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial ); 
-
-scene.add( cube );
-scene.add( ring );
-scene.add( can );
-scene.add( cone );
-scene.add( sphere );
+scene.add( dvd );
 
 var light = new THREE.DirectionalLight('white', 6);
 light.position.set(0,5, 5) .normalize();
 scene.add(light);
 
-cube.position.x -= 4;
-cube.position.y += 2;
-cube.position.z += 0.05;
+// variables
 
-ring.rotation.x += 0.8;
-ring.rotation.y += 0.8;
+colCount = 0;
 
-ring.position.x += 3;
-ring.position.y += 1.7;
-ring.position.z += 0.05;
+const colorRandom = ["#42f587", "#4254f5", "#4254f5", "#f55742", "#d742f5"];
+dvdColor = colorRandom[getRandomInt(5)];
+newDvdColor = dvdColor;
 
-can.position.x -= 3;
-can.position.y -= 2;
-can.position.z += 0.05;
+const maxXSpeed = [0.0025, -0.0025];
+const maxYSpeed = [0.0015, -0.0015];
 
-cone.position.x += 3;
-cone.position.y -= 1.7;
-cone.position.z += 0.05;
+xSpeed = maxXSpeed[getRandomInt(2)];
+ySpeed = maxYSpeed[getRandomInt(2)];;
+Rot = 0.02;
 
-sphere.position.x -= 0;
-sphere.position.y += 0.1;
-sphere.position.z += 0.05;
+if (xSpeed == -0.0025){
+    Rot = 0.02;
+    console.log("left");
+}
+else if (xSpeed == 0.0025){
+    Rot = -0.02;
+    console.log("right");
+}
+
+
+// initialize properties
+dvd.material.color.set(dvdColor);
+
+dvd.rotation.x += 0.1;
+dvd.rotation.z += 0.5;
 
 function animate() {
 	requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    
+    dvd.rotation.y += Rot;
+    dvd.rotation.z += Rot;
 
-    ring.rotation.y += 0.1;
+    dvd.position.x += xSpeed;
+    dvd.position.y += ySpeed;
 
-    can.rotation.x += 0.02;
-    can.rotation.z += 0.05;
+    if (dvd.position.x > 0.95 || dvd.position.y > 0.85 || dvd.position.x < -0.90 || dvd.position.y < -0.90){
+        while (dvdColor == newDvdColor){
+            dvdColor = colorRandom[getRandomInt(5)];
+        }
+        newDvdColor = dvdColor;
+        dvd.material.color.set(dvdColor);
+        colCount ++;
+        console.log("col = " + colCount);
+    }
 
-    cone.rotation.x -= 0.02;
+    if (dvd.position.x > 0.95){
+        xSpeed = -0.0025
+        Rot = -0.02;
+        dvd.scale.x -= 0.12;
+        dvd.scale.y -= 0.12;
+    }
+    else if (dvd.position.y > 0.85){
+        ySpeed = -0.0015
+        Rot = 0.02;
+        dvd.scale.x -= 0.12;
+        dvd.scale.y -= 0.12;
+    }
+    else if (dvd.position.x < -0.90){
+        xSpeed = 0.0025
+        Rot = -0.02;
+        dvd.scale.x -= 0.12;
+        dvd.scale.y -= 0.12;
+    }
+    else if (dvd.position.y < -0.90){
+        ySpeed = 0.0015
+        Rot = 0.02;
+        dvd.scale.x -= 0.12;
+        dvd.scale.y -= 0.12;
+    }
+    else if (colCount >= 8){
+        xSpeed = 0;
+        ySpeed = 0;
+        dvd.scale.x = 0;
+        dvd.scale.y = 0;
+        colCount = 0;
+    }
+
 	renderer.render( scene, camera );
 }
 animate();
